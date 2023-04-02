9a9fdedfe9b4d724245a40dbaaff0b575fe76857
Artem Stepanenko <stpnnk.artem@gmail.com>
2023-04-02 17:49:23 +0300

    wip

--- src/Database/Migrations/Migration.php
+++ src/Database/Migrations/Migration.php
@@ -3,13 +3,13 @@
 namespace Stepanenko3\LaravelApiSkeleton\Database\Migrations;
 
 use Illuminate\Database\Migrations\Migration as MigrationsMigration;
-use Illuminate\Database\Schema\MySqlBuilder;
 use Illuminate\Support\Facades\DB;
 use Stepanenko3\LaravelApiSkeleton\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
 
 class Migration extends MigrationsMigration
 {
-    public function schema(): MySqlBuilder
+    public function schema(): Builder
     {
         $schema = DB::connection()->getSchemaBuilder();
         $schema->blueprintResolver(fn ($table, $callback) => new Blueprint($table, $callback));
